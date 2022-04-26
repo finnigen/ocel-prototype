dddf4bb2d821f0f38736ee4645b76351b9b17623
Louis Herrmann <louis.herrmann@yahoo.de>
2022-04-26 18:29:11 +0200

    added interleaved miner

--- Project/operators.py
+++ Project/operators.py
@@ -1,7 +1,7 @@
 import copy
 import json
 import ocel as ocel_lib
-
+import datetime
 
 
 # input: 2 ocels, object relationship, attribute name(s) from ocels
@@ -81,5 +81,82 @@ def manualMiner(log1, log2, object_relation, activity_relation):
         raise Exception("INVALID OCEL DOES NOT MATCH SCHEMA")
         
     print("successful validation of new log")
+    
+    return newLog
+
+
+
+
+# interleaved miner section start
+def sortByTimeStamp(element):
+    return element[2]["ocel:timestamp"]
+
+
+# calculates interleaved event relation of two logs, used as helper for interleaved miner
+def interleavedRelation(log1, log2, obj1, obj2):
+    # calculate path from obj1 and obj2
+    path = []
+    for ev_id, event in log1["ocel:events"].items():
+        if obj1 in event["ocel:omap"]:
+            path.append((1, ev_id, event))
+    
+    for ev_id, event in log2["ocel:events"].items():
+        if obj2 in event["ocel:omap"]:
+            path.append((2, ev_id, event))
+    
+    path.sort(reverse=False, key=sortByTimeStamp)
+    
+    matchingEvents = set()
+    
+    for i in range(len(path)-1):
+        if path[i][0] != path[i+1][0]:
+            matchingEvents.add((path[i][:2], path[i+1][:2]))
+    
+    # returns set of relation tuples. Each tuple contains two tuples, 
+    # first element of inner tuple indicates which log, second element indicates event id
+    return matchingEvents
+
+
+# interleaved miner
+def interLeavedMiner(log1, log2, object_relationship):
+    # start with log1 since we want all its events and objects
+    newLog = copy.deepcopy(log1)
+    
+    newObjEventPairs = set()
+    
+    log1_objects = log1["ocel:objects"].keys()
+    log2_objects = log2["ocel:objects"].keys()
+    
+    # restrict object relationship to objects in the logs
+    crt_prd = [(x,y) for x in log1_objects for y in log2_objects]
+    reduced_object_relationship = set(crt_prd).intersection(object_relationship)
+    
+    interleavedDict = {}
+    for relation in reduced_object_relationship:
+        interleavedDict[relation] = interleavedRelation(log1, log2, relation[0], relation[1])
+    
+    for ev_id1, event1 in log1["ocel:events"].items():
+        for obj1 in event1["ocel:omap"]:
+            for relation in reduced_object_relationship:
+                if relation[0] == obj1:
+                    obj2 = relation[1]
+                    for ev_id2, event2 in log2["ocel:events"].items():
+                        if obj2 in event2["ocel:omap"]:
+                            if ((1, ev_id1), (2, ev_id2)) in interleavedDict[(obj1, obj2)]:
+                                newObjEventPairs.add((ev_id1, obj2))
+
+    # add new pairs to newLog
+    for pair in newObjEventPairs:
+        ev_id = pair[0]
+        obj = pair[1]
+        
+        # add new object to event omap, but avoid duplicates
+        objects = newLog["ocel:events"][ev_id]["ocel:omap"]
+        objects.append(obj)
+        newLog["ocel:events"][ev_id]["ocel:omap"] = list(set(objects))
+        
+        # add new object to objects of log
+        if obj not in newLog["ocel:objects"].keys():
+            newLog["ocel:objects"][obj] = log2["ocel:objects"][obj]
     
     return newLog
